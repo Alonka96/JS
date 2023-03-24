20d83896d5f0aefe745d8432eacf564e834c880a
Alona Lysenko <alona.lysenko96@gmail.com>
2023-03-24 17:28:32 +0200

    Practice

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-let a = 4;
-let b = 'four';
-let c = null;
-let d = undefined;
-let e = true;
+let offer = prompt ('Оберіть напій \n1 - Tea \n2 - Coffee \n3 - juice');
+
+if( offer === '1') {
+  console.log("you chose tea");
+} else if( offer === '2') {
+  console.log("you chose coffee");
+} else if (offer === '3') {
+  console.log('you chose juice');
+} else {
+  console.log('no drink');
+}
