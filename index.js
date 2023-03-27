cefb3e17dfddc051616160e59e246e60758391a7
Alona Lysenko <alona.lysenko96@gmail.com>
2023-03-27 16:03:24 +0300

    Practice for

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-// let counter = 0;
-// while(counter<=10) {
-//   if(counter % 2 === 0) {
-//     console.log(counter);
-//   }
-//   counter++;
-// }
-
-
-// let number = 0;
-// while(number<=15) {
-//   if((number % 3 === 0) && (number % 5 === 0)) {
-//     console.log(number);
-//   }
-//   number++;
-// }
-
-
-
-let sum = 0;
-let counter = 0;
-
-while(counter <= 20) {
-  sum += counter;
-  counter++;
+function fizzBuzz(num){
+  for(let i = 1; i <= num; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log('FizzBuzz');
+    } else if (i % 3 ===0){
+      console.log('Fizz');
+    } else if (i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
 }
-console.log(sum);
