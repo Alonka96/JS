9c9eb8918ad29ae512a5ca2f50396e4a115c469e
Alona Lysenko <alona.lysenko96@gmail.com>
2023-03-29 21:07:22 +0300

    пусте непусте

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const userPlan = {
-  mon: "dantist",
-  tue: "go to park",
-  wed: "hard work",
-  thu: "go party",
-  fri: "read a book",
-  sat: "stay in bed",
-};
-
-
-const userInput = prompt("What day do you want?");
-alert(`${userPlan[userInput] ? userPlan[userInput] : 'Noting planned'}`);
+function isEmpty(obj) {
+  for (const key in object) {
+    if(obj[key]) {
+      return false;
+    }
+  }
+  return true;
+}
