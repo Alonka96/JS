f324a153d46bffb197f35afe18406fdef472e0d0
Alona Lysenko <alona.lysenko96@gmail.com>
2023-03-30 17:53:18 +0300

    функція-конструктор

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-function isEmpty(obj) {
-  for (const key in object) {
-    if(obj[key]) {
-      return false;
+function Car(brand, maxSpeed = 200) {
+  this.brand = brand;
+  this.maxSpeed = maxSpeed;
+  this.speed = 0;
+
+  this.accelerate = function (value) {
+    this.speed += value;
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
     }
-  }
-  return true;
+    return this.speed;
+  };
+
+  this.deaccelerate = function (value) {
+    this.speed -= value;
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
+    return this.speed;
+  };
+
+  this.stop = function () {
+    this.speed = 0;
+    return this.speed;
+  };
 }
+
+const bmv = new Car("BMV", 250);
