cdf39ca1d14a99414c9a099928b786c0a4e4cb51
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-04 18:13:40 +0300

    practice  this

--- index.js
+++ index.js
@@ -1,47 +1,18 @@
-const arr = [1, 4, 8, 9];
-
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
-
-let sum = 0;
-for (let i = 0; i < arr.length; i++) {
-  sum += arr[i];
-}
-console.log(sum);
-
-function getMaxElement(array) {
-  let max = array[0];
-  for (let i = 1; i < array.length; i++) {
-    if (max < array[i]) {
-      max = array[1];
-    }
+function Worker(firstName, lastName, rate, days) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  if (rate < 0) {
+    this.rate = 0;
+  } else {
+    this.rate = rate;
   }
-  return max;
-}
-
-function getMinElement(array) {
-  let min = array[0];
-  for (let i = 1; i < array.length; i++) {
-    if (min > array[i]) {
-      min = array[1];
-    }
+  if ((days = 0)) {
+    this.days = 0;
+  } else {
+    this.days = days;
   }
-  return min;
-}
 
-function getAverage(arr) {
-  let sum = 0;
-  for (let i = 1; i < array.length; i++) {
-    sum += arr[i];
-  }
-  return sum / array.length;
-}
-
-function getSumOfEven(array) {
-  let sum = 0;
-  for (let i = 1; i < array.length; i = i + 2) {
-    sum += array[i];
-  }
-  return sum;
+  this.getSalary = function () {
+    return this.days * this.rate;
+  };
 }
