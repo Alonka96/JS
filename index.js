508c30fc4919d6bd6d1381f6ab5826607d10180b
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-06 17:33:40 +0300

    set and get

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 class Worker {
   constructor(name, rate, days) {
     this.name = name;
-    this.rate = rate;
-    this.days = days;
+    if (rate < 0) {
+      throw new RangeError("Rate must be more than zero");
+    } else {
+      this._rate = rate;
+    }
+    if (days < 0) {
+      throw new RangeError("Days must be more than zero");
+    } else {
+      this._days = days;
+    }
   }
+
+  // setRate(value) {
+  //   if (typeof value !== "number") {
+  //     throw new TypeError("Rate must be a number");
+  //   }
+  //   if (value < 0) {
+  //     throw new RangeError("Rate must be more than zero");
+  //   }
+  //   this._rate = value;
+  // }
+  set rate(value) {
+    // тут необхідні перевірки
+    this._rate = value;
+  }
+
+  get rate() {
+    return this._rate;
+  }
+
+  // setDays(value) {
+  //   if (typeof value !== "number") {
+  //     throw new TypeError("Days must be a number");
+  //   }
+  //   if (value < 0) {
+  //     throw new RangeError("Days must be more than zero");
+  //   }
+  //   this._days = value;
+  // }
+
+set days(value) {
+  // перевіряємо передане значення на правильність
+  this._days = value;
+}
+get days() {
+  return this._days;
+}
 
   getSalary() {
     return this.rate * this.days;
