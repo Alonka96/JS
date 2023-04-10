eceb258ee855123fe6240907a88073cdee00a891
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-10 19:15:51 +0300

    reduce

--- index.js
+++ index.js
@@ -1,48 +1,8 @@
-const newspaper = {
-  title: "My super news",
-  articles: [
-    {
-      author: "Van Doe",
-      header: "Article1",
-      body: "lorem ipsum",
-    },
-    {
-      author: "John Doe",
-      header: "Article2",
-      body: "dolor sit amet",
-    },
-    {
-      author: "Jane Doe",
-      header: "Article3",
-      body: "lorem ipsum",
-    },
-    {
-      author: "Alex Doe",
-      header: "Article4",
-      body: "dolor sit amet",
-    },
-    {
-      author: "Carl Doe",
-      header: "Article5",
-      body: "lorem ipsum",
-    },
-    {
-      author: "Miky Doe",
-      header: "Article6",
-      body: "dolor sit amet",
-    },
-    {
-      author: "Ricky Doe",
-      header: "Article7",
-      body: "lorem ipsum",
-    },
-  ],
-  showArticles() {
-    this.articles.forEach(logItems.bind(this));
-  },
-};
-function logItems(currentArticle, index) {
-  console.log(
-    `${this.title} --- ${index} : ${currentArticle.author} - ${currentArticle.header}`
+const sum = (a, b, c) => a + b + c;
+function sumAll() {
+  const argArray = Array.from(arguments);
+  const res = argArray.reduce(
+    (accumulator, current) => accumulator + current + 0
   );
+  return res;
 }
