9a679352bb7a00be99990a68d1ee44632a7302ca
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-11 17:22:00 +0300

    double cycle

--- index.js
+++ index.js
@@ -1,43 +1,8 @@
-const monitor = {
-  sizes: {
-    length: {
-      value: 20,
-      scale: "sm",
-    },
-    width: {
-      value: 30,
-      scale: "sm",
-    },
-  },
-  brightness: "2000lm",
-  color: "black",
-  manufacturer: {
-    name: "Sony",
-    address: {
-      city: "London",
-      street: "Baker",
-      house: 27,
-    },
-  },
-};
-
-/// d - Math.sqrt(a*a + b*b);
-// const diagonal = Math.sqrt(
-//   monitor.sizes.length.value * monitor.sizes.length.value + monitor.sizes.width.value * monitor.sizes.width.value);
-///вирішення проблеми>
-// const lengthValue = monitor.sizes.length.value;
-// const widthValue = monitor.sizes.width.value;
-// const diagonal = Math.sqrt(lengthValue * lengthValue + widthValue * widthValue);
-console.log(diagonal);
-
-const manufacturerCity = monitor.manufacturer.address.sity;
-///деструктурування (розібрання на елементи)
-const {
-  sizes: {
-    length: { value: lengthValue },
-    width: { value: widthValue },
-  },
-  manufacturer: {
-    address: { city },
-  },
-} = monitor;
+let str = "";
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    str += "+";
+  }
+  str += "\n";
+}
+console.log(str);
