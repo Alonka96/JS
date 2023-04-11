9354e142516dbf0180ebcbfe40af8a7c88779638
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-11 13:05:23 +0300

    spred task resolve

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-
-const sumAllArrow = (...array) => {
-  const sum = array.reduce((accumulator, current) => accumulator + current, 0);
+function sumAll(...restArr) {
+  let sum = 0;
+  for (let i = 0; i < restArr.length; i++) {
+    sum += restArr[i];
+  }
   return sum;
-};
+}
+
+const arr1 = [1, 3, 5, 7];
+const arr2 = [2, 4, 6, 8];
+const concatedArray = [...arr1, ...arr2]; 
