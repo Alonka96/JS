10d8e755f0b9040bd02d23b20cf69181e6cfd263
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-11 17:59:52 +0300

    double cycle square

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let str = "";
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j <= i; j++) {
-    str += "*";
+function drowEmptySquare(dim) {
+  let str = "";
+  for (let i = 0; i < dim; i++) {
+    for (let j = 0; j < dim; j++) {
+      if (i === 0 || i === dim - 1 || j === dim - 1) {
+        str += "*";
+      } else {
+        str += " ";
+      }
+    }
+    str += "\n";
   }
-  str += "\n";
-}
 
-console.log(str);
+  console.log(str);
+}
+drowEmptySquare(8);
