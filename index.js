baed2e7a775842498ad44e06a88ecae7652b1800
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-17 18:44:26 +0300

    Friend cash task resolve recursia

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-function factorial(num) {
-  if (num === 1) {
-    return 1;
+class Friend {
+  /**
+   * @param {String} name - Name of friend
+   * @param {Number} amountCash - Amount of cash
+   * @param {Friend} friend - friend of mine
+   */
+  constructor(name, amountCash, friend) {
+    this.name = name;
+    this.amountCash = amountCash;
+    this.friend = friend;
   }
-  const res = num * factorial(num - 1);
-  return res;
+  getAllCash() {
+    if (this.friend === null) {
+      return this.amountCash;
+    }
+    if (this.friend instanceof Friend) {
+      return this.amountCash + this.friend.getAllCash();
+    }
+    if (this.friend instanceof Array) {
+      return this.friend.reduce((accumulator, friend) => accumulator + this.friend.getAllCash(),0);
+    }
+  }
 }
 
-function func1() {
-  console.log("1");
-  func2();
-}
-function func2() {
-  console.log("2");
-  func3();
-}
-function func3() {
-  console.log("3");
-}
+const Fr = new Friend("Vik", 50, null);
+const I = new Friend("Alice", 20, null);
 
+const frfr = new Friend("Frfr", 50, null);
+
+const friendOfAll = Friend("Kay", 50, [Fr, I, frfr]);
