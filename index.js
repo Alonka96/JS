3cbf0aa4332a97080e7400966d5ac330e5a5f733
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-20 13:17:37 +0300

    createAdder(2var) zamukannya

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-class Friend {
-  /**
-   * @param {String} name - Name of friend
-   * @param {Number} amountCash - Amount of cash
-   * @param {Friend} friend - friend of mine
-   */
-  constructor(name, amountCash, friend) {
-    this.name = name;
-    this.amountCash = amountCash;
-    this.friend = friend;
-  }
-  getAllCash() {
-    if (this.friend === null) {
-      return this.amountCash;
-    }
-    if (this.friend instanceof Friend) {
-      return this.amountCash + this.friend.getAllCash();
-    }
-    if (this.friend instanceof Array) {
-      return this.friend.reduce((accumulator, friend) => accumulator + this.friend.getAllCash(),0);
-    }
-  }
-}
+// function createAdder(n) {
+//   return function (m) {
+//     return (n = n + m);
+//   };
+// }
 
-const Fr = new Friend("Vik", 50, null);
-const I = new Friend("Alice", 20, null);
-
-const frfr = new Friend("Frfr", 50, null);
-
-const friendOfAll = Friend("Kay", 50, [Fr, I, frfr]);
+const createAdder = (n) => (m) => (n = n + m);
+// const innerFun = createAdder(5);
