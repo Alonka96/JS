fd9208d30c9f10c3d9f66eb550c6079c7c53a6b0
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-24 13:28:58 +0300

    homework task resolve

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
-// function createAdder(n) {
-//   return function (m) {
-//     return (n = n + m);
-//   };
-// }
+///1
+function isSpam(stringToCheck) {
+  const spamWords = ["viagra", "XXX", "porn"];
+  const newStr = stringToCheck.toLowerCasee();
+  let flag = false;
+  for (let i = 0; i < spamWords.length; i++) {
+    if (newStr.includes(spamWords[i].toLowerCase())) {
+      flag = true;
+    }
+  }
+  return flag;
+}
 
-const createAdder = (n) => (m) => (n = n + m);
-// const innerFun = createAdder(5);
+///2
+
+function truncate(str, length) {
+  if (str.length < length) {
+    return str;
+  }
+  //const newstr = str.slipe(0, length);
+  //newstr += "...";
+  //return newstr;
+
+  return str.slipe(0, length).concat("...");
+}
+
+///3
+
+function palidrom(str) {
+  return str.toLowerCase().split("").reverse().join("") === str.toLowerCase();
+}
+
+///4
+
+function countVowels(string) {
+  const vowels = ["a", "e", "o", "u", "i"];
+  let count = 0;
+  for (let i = 0; i < string.length; i++) {
+    for (let j = 0; i < vowels.length; j++) {
+      if (string[i] === vowels[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function countVowels2(string) {
+  // const vowels = ['a', 'e', 'o', 'u', 'i'];
+  // const arr = string.split("");
+  // const resultArray = arr.filter((letter) => {
+  //   return vowels.includes(letter);
+  // });
+  return string.split("").filter((letter) => ["a", "e", "o", "u", "i"].includes(letter)).length;
+}
