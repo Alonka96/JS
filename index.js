66233688f53ac576a42a1ed53ad4c9bad9ea2fb9
Alona Lysenko <alona.lysenko96@gmail.com>
2023-04-28 20:57:15 +0300

    guessNumber task

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
-class LinkedItem {
-  constructor(value) {
-    this._value = value;
-    this.next = null;
-    this.prev = null;
-  }
-  get value() {
-    return this._value;
-  }
-  set value(v) {
-    this._value = v;
-  }
-}
-
-class LinkedList {
-  constructor(...args) {
-    this.length = 0;
-    this.head = null;
-    this.tail = null;
-    // for (let i = 0; i < args.length; i++) {
-    //   this.push(args[i]);
-    // }
-    args.forEach((el) => {this.push(el);
-    });
-  }
-
-  push(value) {
-    const item = new LinkedItem(value);
-    if (this.length === 0) {
-      ///якщо він перший
-      this.head = item;
-      this.tail = item;
-    } else {
-      ///якщо хтось вже стоїть
-      this.tail.prev = item;
-      item.next = this.tail;
-      this.tail = item;
+function guesNumber() {
+  alert("Figure number from 0 to 100");
+  let start = 0;
+  let end = 100;
+  let middle = Math.round((start + end) / 2);
+  while (true) {
+    if (confirm(`Your number is ${middle}?`)) {
+      return middle;
     }
-    return ++this.length;
+    if (confirm(`${middle} > than your number?`)) {
+      end = middle;
+      middle = Math.floor((start + end) / 2);
+    } else {
+      start = middle;
+      middle = Math.ceil((start + end) / 2);
+    }
   }
 }
