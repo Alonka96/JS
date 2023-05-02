6e6d4bbf2dacf6bbbcb2f8ebee052867c46beedc
Alona Lysenko <alona.lysenko96@gmail.com>
2023-05-02 12:42:44 +0300

    Stack realisation

--- index.js
+++ index.js
@@ -1,86 +1,43 @@
-const nums = [1, 2, 1, 4, 7, 3, 1, 9, 3, 2, 4, 6, 4, 3, 1];
-// приклад алгоритму лінійної складності:
-// скільки вхідних даних - стільки і операцій
-// O(n)
-const linearSearch = (elem, arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === elem) {
-      return i;
-    }
+class Stack {
+  constructor(maxSize, ...array) {
+    this._maxSize = maxSize;
+    this._size = 0;
+    array.forEach((el) => {
+      this.push(el);
+    });
   }
-  return -1;
-};
-// приклад алгоритму квадратичної складності
-// вхідні дані в квадраті = стільки операцій і буде виконано
-// O(n^2)
-const multyTable = (limit) => {
-  const table = [];
-  for (let i = 1; i <= limit; i++) {
-    for (let j = 1; j <= limit; j++) {
-      table.push(`${i}*${j}=${i * j}`);
-    }
-  }
-  return table;
-};
-//// Бінарний пошук
-/// Працює тільки на відсортованих даних
-/// логаріфмічна залежність
-const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const binarySearch = (array, whatToFind) => {
-  debugger;
-  let start = 0;
-  let end = array.length - 1;
-  let middle = Math.round((start + end) / 2);
-  while (true) {
-    if (array[middle] === whatToFind) {
-      return middle;
-    }
-    if (array[middle] > whatToFind) {
-      end = middle;
-      middle = Math.floor((start + end) / 2);
-    }
-    if (array[middle] < whatToFind) {
-      start = middle;
-      middle = Math.ceil((start + end) / 2);
-    }
-  }
-};
-//// Бінарний пошук - рекурсія
-const recursiveBinarySearch = (array, whatToFind) => {
-  const middle = Math.ceil((array.length - 1) / 2);
-  debugger;
-  if (array[middle] === whatToFind) {
-    return middle;
-  } else if (array[middle] > whatToFind) {
-    return recursiveBinarySearch(array.slice(0, middle), whatToFind);
-  } else if (array[middle] < whatToFind) {
-    return recursiveBinarySearch(array.slice(middle, array.length), whatToFind);
-  } else {
-    return -1;
-  }
-};
 
-/*
-Таска на "угадайку" - користувач загадує подумки число, ваша задача - написати функцію, яка його відгадує
-Число від 0 до 100
-Для підтвердження або спростування використовувати confirm
-*/
+  get size() {
+    return this._size;
+  }
 
-function guessNumber() {
-  alert("Figure number from 0 to 100");
-  let start = 0;
-  let end = 100;
-  let middle = Math.round((start + end) / 2);
-  while (true) {
-    if (confirm(`Your number is ${middle}?`)) {
-      return middle;
+  get isEmpty() {
+    return this._size === 0;
+  }
+
+  push(value) {
+    if (this.size >= this._maxSize) {
+      throw new RangeError("Stack overflow");
     }
-    if (confirm(`${middle} > than your number?`)) {
-      end = middle;
-      middle = Math.floor((start + end) / 2);
-    } else {
-      start = middle;
-      middle = Math.ceil((start + end) / 2);
+    this[`_${this.size}`] = value;
+    this._size++;
+    return this._size;
+  }
+
+  pop() {
+    /*Витягти останній елемент стеку, відкласти в окрему змінну, видалити останній елемент стеку, зменшити розмір, повернути значення*/
+    if (this.isEmpty) {
+      return null;
     }
+
+    const lastItem = this[`_${this.size - 1}`];
+    delete this[`_${this.size - 1}`];
+    this._size--;
+    return lastItem;
+  }
+
+  pick() {
+    /*Повернути значення останнього елемента стеку, не видаляючи його*/
+    return this[`_${this.size - 1}`];
   }
 }
