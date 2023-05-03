5a81beb6834a0fcd40a9d70b19c079d051388ad5
Alona Lysenko <alona.lysenko96@gmail.com>
2023-05-03 12:28:01 +0300

    symbol

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
-class Stack {
-  constructor(maxSize, ...array) {
-    this._maxSize = maxSize;
-    this._size = 0;
-    array.forEach((el) => {
-      this.push(el);
-    });
-  }
+/ Symbol
 
-  get size() {
-    return this._size;
-  }
+/*
+const symb = new Symbol();
+//Uncaught TypeError: Symbol is not a constructor
+const symb = Symbol();
+//undefined
+symb;
+//Symbol()
+const symb2 = Symbol('label for human');
+//undefined
+symb2
+//Symbol(label for human)
+const symb3 = Symbol();
+//undefined
+symb === symb3;
+//false
 
-  get isEmpty() {
-    return this._size === 0;
-  }
+*/
 
-  push(value) {
-    if (this.size >= this._maxSize) {
-      throw new RangeError("Stack overflow");
-    }
-    this[`_${this.size}`] = value;
-    this._size++;
-    return this._size;
-  }
 
-  pop() {
-    /*Витягти останній елемент стеку, відкласти в окрему змінну, видалити останній елемент стеку, зменшити розмір, повернути значення*/
-    if (this.isEmpty) {
-      return null;
-    }
 
-    const lastItem = this[`_${this.size - 1}`];
-    delete this[`_${this.size - 1}`];
-    this._size--;
-    return lastItem;
-  }
+const symb = Symbol();
 
-  pick() {
-    /*Повернути значення останнього елемента стеку, не видаляючи його*/
-    return this[`_${this.size - 1}`];
-  }
+const obj = {
+    userName: 'John',
+    [symb]: 'Hello'
 }
+
+console.log(obj[symb]);
