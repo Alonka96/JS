82b4a5a1d96cd237a1993e09b1cce94845347bed
Alona Lysenko <alona.lysenko96@gmail.com>
2023-05-04 14:39:29 +0300

    Messages dialog Map

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function translater(stringToTranslate) {
-  const vocabulary = new Map();
-  vocabulary.set("cat", "кіт");
-  vocabulary.set("eat", "їсти");
-  vocabulary.set("dog", "собака");
+const user1 = {
+  firstName: "John",
+  lastName: "Doe",
+};
 
-  const words = stringToTranslate.toLowerCase().split(" ");
-  const translatedArray = words.map((word) => vocabulary.has(word) ? vocabulary.get(word) : word
-  );
-  return translatedArray.join(" ");
-}
+const user2 = {
+  firstName: "Jane",
+  lastName: "Crow",
+};
+
+const johnMessages1 = ["hello", "how are you"];
+const johnMessages2 = ["hi!", "Im fine, thank you"];
+
+const dialogMap = new Map();
+dialogMap.set(user1, johnMessages1);
+dialogMap.set(user2, johnMessages2);
